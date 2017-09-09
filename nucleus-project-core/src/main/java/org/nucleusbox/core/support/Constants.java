4074e1f2e9ecdf2f766d5304113f7b709f51b685
Brijesh Singh <brijesh.caet@gmail.com>
2017-09-09 12:42:36 +0530

    Rename package

--- nucleus-project-core/src/main/java/org/nucleusbox/core/support/Constants.java
+++ nucleus-project-core/src/main/java/org/nucleusbox/core/support/Constants.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.nucleusbox.core;
+package org.nucleusbox.core.support;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
