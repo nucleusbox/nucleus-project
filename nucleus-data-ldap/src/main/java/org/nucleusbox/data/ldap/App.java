e71f1b31d9f281563887e7f6b41c5527188cee40
Brijesh Singh <brijesh.caet@gmail.com>
2017-09-10 12:48:36 +0530

    Rename Packages

---
+++ nucleus-data-ldap/src/main/java/org/nucleusbox/data/ldap/App.java
@@ -0,0 +1,13 @@
+package org.nucleusbox.data.ldap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ nucleus-data-lucene/src/main/java/org/nucleusbox/data/lucene/App.java
@@ -0,0 +1,13 @@
+package org.nucleusbox.data.lucene;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
